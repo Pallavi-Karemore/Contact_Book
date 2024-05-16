a74c45b109065b9687298d60740ebc57668dc5a6
Pallavi Karemore <pallavikaremore7@gmail.com>
2024-05-16 13:00:41 +0530

    My commit

---
+++ contact_Book.py
@@ -0,0 +1,19 @@
+from contact import Contact
+class ContactBook:
+    def __init__(self):
+        self.contact=[]
+    def add_contact(self,contact):
+        self.contact.append(contact)
+        
+    def display_all_contact(self):
+        for contact in self.contact:
+            contact.display_contact()
+            print("------------------")
+
+    def search_contact(self,name):
+        for contact in self.contact:
+            if contact.name.lower()==name.lower():
+                return contact
+        else:
+            return None       
+
